eb4b22b19d877255b7d4a018336313acd1f4b95d
Ethan Buchanan <Ethan.leslieb@gmail.com>
2022-09-14 17:57:20 -0600

    completed prototypes challenge

--- index.js
+++ index.js
@@ -15,10 +15,29 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+this.name = name;
+this.age = age;
+this.stomach = [];
+}
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
 }
 
+// const mary = new Person('Mary', 50);
+// console.log(mary.toString());
+// mary.eat('pizza')
+// console.log(mary.stomach)
+// mary.poop()
+// console.log(mary.stomach)
 
 /*
   TASK 2
@@ -36,9 +55,33 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon
+this.tank = 0;
+this.odometer = 0;
+}
+
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+
+Car.prototype.drive = function(distance){
+const driveableMiles = this.tank * this.milesPerGallon;
+if(distance <= driveableMiles){
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - (distance / this.milesPerGallon)
+}else{
+  this.odometer = this.odometer + driveableMiles;
+  this.tank = 0;
+  return `I ran out of fuel at ${this.odometer} miles`;
+}
+
 
 }
+
+
+
 
 
 /*
@@ -49,18 +92,22 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. window pulls from the global object which could be anything written in the code of the program, shouldnt be used
+  2. created a context and this will bind to whatever is left of the dot (principle)
+  3. explicit binding will save a specific invocation together with an object or context where 'this' will be used for. 
+  4. new binding is creating individual copies of a template that can be modified slightly
 */
 
 ///////// END OF CHALLENGE /////////
